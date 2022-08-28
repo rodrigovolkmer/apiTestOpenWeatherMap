d04202570afb88c1f569321533aff7f42445e02d
Rodrigo Volkmer <rodrigo.volkmer@rightcrowd.com>
2022-08-29 07:36:02 +1000

    wip#3

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,16 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
 	e2e: {
 		baseUrl: 'https://api.openweathermap.org',
+    viewportWidth: 1300,
+    viewportHeight: 1000,
+    requestTimeout: 20000,
+    defaultCommandTimeout: 20000,
+    pageLoadTimeout: 20000,
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'cypress/results/test-results-[hash].xml',
+      toConsole: true,
+    },
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
